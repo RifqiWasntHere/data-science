e70763c4723b040c9d19513dcd766ab96a32579d
RifqiNotWibu <rifqifadhillahh@gmail.com>
2022-11-26 13:35:02 +0700

    Moved old files into folder [Prototype]

--- Prototype/hitungLabel.py
+++ Prototype/hitungLabel.py
@@ -21,20 +21,3 @@ def hitungLabel(arrayName, arrayName2):
       elif ( arrayName2[i] == "vgood" ):
         d += 1
   total = a + b + c + d
-  
-def hitungLugage(arrayName1, arrayName2):
-  global a, b, c, d, total
-  a, b, c, d = 0, 0, 0, 0
-
-  for i in range(len(arrayName2)):
-    if(arrayName1[i] == True):
-      if ( arrayName2[i] == "unacc"):
-        a += 1
-      elif ( arrayName2[i] == "acc"):
-        b += 1
-      elif ( arrayName2[i] == "good"):
-        c += 1
-      elif ( arrayName2[i] == "vgood"):
-        d += 1
-
-  total = a + b + c + d
